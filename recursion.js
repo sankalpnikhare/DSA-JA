c5d812e2ecf4b3b3189b090654d8f4293147b174
Sankalp Nikhare <sankalpnikhare023@gmail.com>
2026-08-07 17:32:00 +0530

    d

--- recursion.js
+++ recursion.js
@@ -1,13 +1,21 @@
-//Function calling itself 
-function fun(num){
-    if (num==0){
-        return ; 
-    } 
-    console.log(num);
+// //Function calling itself 
+// function fun(num){
+//     if (num==101){
+//         return ; 
+//     } 
+//     console.log(num);
     
     
-    fun(num-1);
+//     fun(num+1);
+// }
+// let n = 1 ; 
+// fun(n);
+function sum(n){
+    if (n==0) return 0 ;
+    return n + sum(n-1);
 }
+console.log(sum(5));
 
-fun(5);
+// console.log("Exprected value" , 5*(6) / 2);
+
 
