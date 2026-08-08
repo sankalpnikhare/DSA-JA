226143eba65caa870df412c77d2686bbc4f13e98
Sankalp Nikhare <sankalpnikhare023@gmail.com>
2026-08-08 15:37:35 +0530

    modifiy

--- remove_zeroes.js
+++ remove_zeroes.js
@@ -7,6 +7,16 @@ function rem_zero(arr){
     }
     return Object.keys(hash)
 }
+//another way
+function remove_zero(arr){
+    for(let i =  0 ; i < arr.length ; i++){
+        if(arr[i]===0){
+            arr.splice(i ,1 );
+            i-- ; 
+        }
+    }
+    return arr ; 
+}
 
 let arr = [1,2,3,0,4 , 0];
 console.log(remove_zero(arr));
